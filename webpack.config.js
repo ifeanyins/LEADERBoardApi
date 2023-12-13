16d24db5b1b00fdcd9d4cc651b0d9ec009a2ae86
ifeanyi_ns <ifeanyi_ns@icloud.com>
2023-12-13 09:56:42 +0100

    building project foundations

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,40 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: path.resolve(__dirname, 'src/index.js'),
+
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-    }
+    },
+    devServer: {
+        static: {
+           directory: path.resolve(__dirname, 'dist')
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+   plugins: [
+    new HtmlWebpackPlugin ({
+        title:'LeadBoardApi',
+        template:'./src/index.html',
+    })
+   ]
 }
