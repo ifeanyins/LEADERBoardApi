c3e8fd594969845a6f6288a875f2b2d4af083a33
ifeanyi_ns <ifeanyi_ns@icloud.com>
2023-12-21 21:58:53 +0100

    api module file completion

--- module/api.js
+++ module/api.js
@@ -1,2 +1,25 @@
 // first function should be getting the finished api (the api already containing the user and the score)
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/owQWvuBZRBl6oZNoQKqG/scores/'; //API
+/** first function to generate the API */
+const GetScores = async () => {
+    const getScores = await fetch (url).then((res) => res.json());
+    return getScores;
+};
 // second function should be abou the generation of data through the input
+/** second function to generate the API through input */
+
+const NewApiGame = async () =>{
+    const name = document.getElementById("name");
+    const score = document.getElementById("score");
+
+    await fetch(url, {
+        method: 'POST',
+        headers: {
+            'content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            user:name.value,
+            score:score.value
+        })
+    });
+};
